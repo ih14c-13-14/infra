a3972425e947a2584d062d615a3ff4b422ae4f65
na2na-p <na2na@na2na.dev>
2023-05-25 09:42:50 +0900

    refactor

--- azure/review-notify-endpoint/reviewNotify/index.ts
+++ azure/review-notify-endpoint/reviewNotify/index.ts
@@ -4,7 +4,7 @@ const reviewersToDiscordIdMap = {
 	"na2na-p": "443669016536416267",
 	Issiki0416: "885124658393919548",
 	ryota658: "1098067196363690114",
-};
+} as const;
 
 const webhookEndpointUri =
 	"https://discord.com/api/webhooks/1110906264483012713/RTqfKuSrCUOk3dSc8MTM1SObDn-7OPdBsyEtz2ZpcQskddxUkHKzenPs7S02u8bRDbB8";
@@ -27,15 +27,17 @@ const httpTrigger: AzureFunction = async function (
 
 	const bodyOptions = {
 		htmlUrl: payload.pull_request.html_url,
-		issuer: payload.sender.login,
+		avatarUrl: payload.pull_request.user.avatar_url,
+		issuer: payload.pull_request.user.login,
 		title: payload.pull_request.title,
 		description: payload.pull_request.body,
 		requestedReviewer:
 			reviewersToDiscordIdMap[payload.requested_reviewer.login],
-	};
-	console.log(bodyOptions);
+	} as const;
 
 	const discordPayload = {
+		avatar_url: bodyOptions.avatarUrl,
+		username: bodyOptions.issuer,
 		content: `<@${bodyOptions.requestedReviewer}>\nReview Requested by ${bodyOptions.issuer}`,
 		embeds: [
 			{
@@ -43,9 +45,14 @@ const httpTrigger: AzureFunction = async function (
 				description: bodyOptions.description,
 				url: bodyOptions.htmlUrl,
 				color: Math.floor(Math.random() * 16777215),
+				footer: {
+					text: `Requested at ${new Date().toLocaleString("ja-JP", {
+						timeZone: "Asia/Tokyo",
+					})}`
+				}
 			},
 		],
-	};
+	} as const;
 
 	fetch(webhookEndpointUri, {
 		method: "POST",
