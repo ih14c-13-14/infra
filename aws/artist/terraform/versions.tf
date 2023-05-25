6a3d60ef5778c792403d874046ad0bc9d40f0ea3
na2na-p <na2na@na2na.dev>
2023-05-25 19:07:45 +0900

    S3作るところまで

---
+++ aws/artist/terraform/versions.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = "~> 1.4.6"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.67"
+    }
+  }
+	# local backend
+  backend "local" {
+		path = "../terraform.tfstate"
+	}
+}
