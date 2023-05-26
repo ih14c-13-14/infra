5f33955b074b7a8cf060c9c659fca2a419a989fb
na2na-p <na2na@na2na.dev>
2023-05-26 09:42:35 +0900

    事故るので環境名変更

--- aws/artist/terraform/tfstateManagement/main.tf
+++ aws/artist/terraform/tfstateManagement/main.tf
@@ -1,14 +1,14 @@
 module "s3_bucket_module" {
   source      = "./modules/s3"
-  bucket_name = "artist-tfstate"
+  bucket_name = "artist-tfstate-management"
 }
 
 module "dynamodb_dev_table_module" {
   source     = "./modules/dynamodb"
-  table_name = "artist-dev-tfstate-lock"
+  table_name = "artist-dev-tfstate-lock-management"
 }
 
 module "dynamodb_pre_table_module" {
   source     = "./modules/dynamodb"
-  table_name = "artist-pre-tfstate-lock"
+  table_name = "artist-pre-tfstate-lock-management"
 }
